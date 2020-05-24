6d410dd44a38795875018966a21cf3b78ed4717f
csy <2996307927@qq.com>
2020-05-24 19:35:56 +0800

    Site updated: 2020-05-24 19:35:56

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@
     }
 })(jQuery);
 
-if($(".instagram").length) {
+    if($(".instagram").length) {
     require(['/js/photo.js', '/fancybox/jquery.fancybox.js', '/js/jquery.lazyload.js'], function(obj) {
         obj.init();
     });
